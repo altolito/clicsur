c033b6484facca6731839fbf3d917a11e11ae842
Bertrand Laville <altolito@gmail.com>
2026-05-26 15:36:06 +0200

    Add structured scam datasets and improve dataset testing2

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// import { testDataset } from "./lib/testDataset";
+
+// testDataset();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
